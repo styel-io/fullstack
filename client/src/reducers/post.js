8fa0f469413d31e92e718fd0895760fa93243fa5
kimkihong <ganzik83@gmail.com>
2019-08-13 16:02:25 +0900

    Update post.js

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -63,18 +63,13 @@ export default (state = initialState, action) => {
         ),
         loading: false
       };
+
     case ADD_COMMENT:
       return {
         ...state,
-        post: state.posts.,
+        post: { ...state.post, comments: payload },
         loading: false
       };
-    // case ADD_COMMENT:
-    //   return {
-    //     ...state,
-    //     post: { ...state.post, comments: payload },
-    //     loading: false
-    //   };
     case REMOVE_COMMENT:
       return {
         ...state,
